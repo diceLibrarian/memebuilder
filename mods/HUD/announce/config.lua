9874305cff636aaa65907b74fcad2eb7b078e061
diceLibrarian <solon.sandidge@gmail.com>
2022-08-17 21:05:21 -0400

    Fixed up and cringed

--- mods/HUD/announce/config.lua
+++ mods/HUD/announce/config.lua
@@ -1,8 +1,7 @@
 return {
     delay = 1200, -- in seconds
     announcements = {
-	"&eMESECRAFT: &2MeseCraft is currently in alpha stage of development. Expect major changes!",
-	"&eMESECRAFT: &2Join the MeseCraft online community at &ewww.mesecraft.com",
-	"&eMESECRAFT: &2You can submit suggestions, feedback, and bug reports at &ewww.mesecraft.com.",
+	"&eMEMEBUILDER: &2Memebuilderis currently in alpha stage of development. Expect major changes!",
+	"&eMEMEBUILDER: &2Please suggest fixes on Discord at diceLibrarian#0586",
     }
 }
