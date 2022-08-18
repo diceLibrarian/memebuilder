8b8859f2e281348c88d4c1c952c41ff81929c7c1
diceLibrarian <solon.sandidge@gmail.com>
2022-08-18 14:03:53 -0400

    Prettiness

--- mods/HUD/announce/config.lua
+++ mods/HUD/announce/config.lua
@@ -1,7 +1,7 @@
 return {
     delay = 1200, -- in seconds
     announcements = {
-	"&eMEMEBUILDER: &2Memebuilderis currently in alpha stage of development. Expect major changes!",
+	"&eMEMEBUILDER: &2Memebuilder is currently in alpha stage of development. Expect major changes!",
 	"&eMEMEBUILDER: &2Please suggest fixes on Discord at diceLibrarian#0586",
     }
 }
